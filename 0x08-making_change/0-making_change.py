5f920bc44bd134f1631185703c89e2e2c5619be2
Toluwani Ajibare <toluwaniajibare@gmail.com>
2023-09-13 02:02:42 +0100

    solved task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module documentation for 0-making_change.py
+"""
+
+
+def makeChange(coins, total):
+    """
+    A function that  determine the fewest number
+    of coins needed to meet a given amount
+    """
+    if total < 1:
+        return 0
+    coins = sorted(coins, reverse=True)
+    coin_sum = 0
+    count = 0
+
+    for coin in coins:
+        while coin + coin_sum <= total:
+            coin_sum += coin
+            count += 1
+        if coin_sum == total:
+            return count
+    if coin_sum == total:
+        return count
+    return -1
