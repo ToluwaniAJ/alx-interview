41bb7ff37a468a44ee954ce307df1bf771b3d163
Toluwani Ajibare <toluwaniajibare@gmail.com>
2023-09-27 03:04:28 +0100

    corrected code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,30 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
+"""Parsing log files with python"""
 
-if __name__ == '__main__':
+import re
+import sys
 
-    import sys
 
-    def print_results(statusCodes, fileSize):
-        """ Print statistics """
-        print("File size: {:d}".format(fileSize))
-        for statusCode, times in sorted(statusCodes.items()):
-            if times:
-                print("{:s}: {:d}".format(statusCode, times))
+def printf(codes, fsize):
+    """Prints in the required format"""
+    print("File size: {}".format(fsize))
+    for k, v in sorted(codes.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
 
-    statusCodes = {"200": 0,
-                   "301": 0,
-                   "400": 0,
-                   "401": 0,
-                   "403": 0,
-                   "404": 0,
-                   "405": 0,
-                   "500": 0
-                   }
-    fileSize = 0
-    n_lines = 0
 
-    try:
-        """ Read stdin line by line """
-        for line in sys.stdin:
-            if n_lines != 0 and n_lines % 10 == 0:
-                """ After every 10 lines, print from the beginning """
-                print_results(statusCodes, fileSize)
-            n_lines += 1
-            data = line.split()
-            try:
-                """ Compute metrics """
-                statusCode = data[-2]
-                if statusCode in statusCodes:
-                    statusCodes[statusCode] += 1
-                fileSize += int(data[-1])
-            except:
-                pass
-        print_results(statusCodes, fileSize)
-    except KeyboardInterrupt:
-        """ Keyboard interruption, print from the beginning """
-        print_results(statusCodes, fileSize)
-        raise
+fsize = 0
+codes = {'200': 0, '301': 0, '400': 0,
+         '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+try:
+    for i, line in enumerate(sys.stdin, 1):
+        vals = line.split()
+        if len(vals) > 3:
+            code = vals[-2]
+            if code in codes:
+                codes[code] += 1
+            fsize += int(vals[-1])
+            if i % 10 == 0:
+                printf(codes, fsize)
+finally:
+    printf(codes, fsize)
